a581534199b1dfba003a175a231c8344172cd2e3
matwhils <matwhils.pro@gmail.com>
2026-06-12 17:34:27 +0200

    Make DOMContentLoaded listener asynchronous

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const sidebarContainer = document.getElementById('sidebar-container');
     if (!sidebarContainer) return;
 
