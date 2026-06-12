d06394a711808de5a25982fda17294d7140b5122
matwhils <matwhils.pro@gmail.com>
2026-06-12 18:15:10 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    // Remplacez les guillemets ci-dessous par votre URL se terminant par /exec
-    API_URL: "https://script.google.com/macros/s/AKfycbx9MPzvLafm_9haXTkC0s1AeWVZl9s7xPvn6KfcKNa5828umvTUbxnECKHoO5PxHXoA/exec"
-};
