4e942ad4bf6d9d9b226428ebd2455797715f1179
matwhils <matwhils.pro@gmail.com>
2026-06-12 18:14:56 +0200

    Add API_URL configuration to gateway.js
    Added API_URL configuration for Google Apps Script endpoint.

--- gateway.js
+++ gateway.js
@@ -1,3 +1,8 @@
+const CONFIG = {
+    // Remplacez les guillemets ci-dessous par votre URL se terminant par /exec
+    API_URL: "https://script.google.com/macros/s/AKfycbx9MPzvLafm_9haXTkC0s1AeWVZl9s7xPvn6KfcKNa5828umvTUbxnECKHoO5PxHXoA/exec"
+};
+
 (function () {
     // 1. Détection de la page actuelle
     const pathName = window.location.pathname;
